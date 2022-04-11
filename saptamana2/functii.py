439a94f9e2cb16f48112e6fed7123761f548f438
MateiEne <ene.matei21@gmail.com>
2022-04-11 20:54:12 +0300

    added homeworks

---
+++ saptamana2/functii.py
@@ -0,0 +1,24 @@
+# def suma(a: int, b: int = 1) -> (int, int):
+#     """
+#     :param a: primul numar
+#     :param b: al doilea numar
+#     :return: suma, diferenta
+#     """
+#     return a + b, a - b
+#
+#
+# suma_mea, dif = suma(3, 4)
+#
+# print(suma_mea, dif)
+
+
+# def my_function(*lista_de_numere):
+#     suma = 0
+#
+#     for i in lista_de_numere:
+#         suma += i
+#
+#     return suma
+#
+#
+# print(my_function(1, 2, 3, 4, 5, 6, 7))
---
+++ saptamana2/tema2.py
@@ -0,0 +1,50 @@
+def param_sum(*args):
+    sum = 0
+
+    for i in args:
+        if type(i) is int:
+            sum += i
+
+    return sum
+
+
+print(param_sum(2, 4, 'abc'))
+
+
+def sum_to_n(n: int):
+    sum = 0
+    odd_numbers_sum = 0
+    even_numebrs_sum = 0
+
+    for i in range(n):
+        if (i + 1) % 2 == 0:
+            even_numebrs_sum += i + 1
+        else:
+            odd_numbers_sum += i + 1
+
+        sum += i + 1
+
+    return sum, even_numebrs_sum, odd_numbers_sum
+
+
+print(sum_to_n(4))
+
+
+def e_intreg(nr):
+    if type(nr) is int:
+        return True
+
+    return False
+
+
+def intreg():
+    nr = input()
+
+    if e_intreg(int(nr)) is True:
+        print(nr)
+    else:
+        return 0
+
+
+if intreg() == 0:
+    print("nu a fost introdus un numar")
